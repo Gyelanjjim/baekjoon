5a18dcc37e92a19df86ab64b5130c2044c63c368
Gyelanjjim <108852943+Gyelanjjim@users.noreply.github.com>
2023-01-30 17:17:58 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 9448 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (line) => {
+  console.log( line.charCodeAt(0) )
+  rl.close();
+}).on("close", () => {
+  process.exit();
+});
