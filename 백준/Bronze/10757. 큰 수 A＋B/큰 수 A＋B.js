c6439b9306bf25bc92aae35b9b188c0298468e0a
Gyelanjjim <108852943+Gyelanjjim@users.noreply.github.com>
2023-02-01 21:14:34 +0900

    Update 큰 수 A＋B.js
    함수 형태로 수정

--- 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.js
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
 const [a,b] = fs.readFileSync('/dev/stdin').toString().trim().split(" ")
-const sum = BigInt(a) + BigInt(b)
-console.log(`${sum}`)
-// console.log( sum.toString() )
+
+const sum = (str1, str2) => {
+	return `${BigInt(str1) + BigInt(str2)}`
+	// return ( BigInt(str1) + BigInt(str2) ).toString() 
+}
+
+console.log( sum(a, b) )
