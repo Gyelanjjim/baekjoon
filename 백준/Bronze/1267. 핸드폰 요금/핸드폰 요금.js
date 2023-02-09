987d3f6687b9ab723cd15acb69fcc429a96ba25a
Gyelanjjim <108852943+Gyelanjjim@users.noreply.github.com>
2023-02-09 10:44:47 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 120 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+const time = input[1].split(' ').map(Number)
+
+const solution = arr => {
+  let [y,m] = [0, 0]
+  arr.forEach(el => {
+    y += (parseInt(el/30)+1)*10
+    m += (parseInt(el/60)+1)*15
+  })
+    
+  if(y === m){
+    return ["Y M", y].join(' ')
+  }else if(y < m){
+    return ["Y", y].join(' ')
+  }else{
+    return ["M", m].join(' ')
+  }
+}
+
+console.log(solution(time))
