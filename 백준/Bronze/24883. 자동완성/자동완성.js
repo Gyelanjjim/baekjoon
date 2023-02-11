d9ea9d53577335c372ffd4e8a5bb744525efdd23
Gyelanjjim <108852943+Gyelanjjim@users.noreply.github.com>
2023-02-11 10:31:42 +0900

    [Bronze IV] Title: 자동완성, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/24883. 자동완성/자동완성.js
+++ 백준/Bronze/24883. 자동완성/자동완성.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim()
 
 const solution = (input) => {
-  const re = new RegExp(/[nN]/g)
+  const re = new RegExp(/[nN]/)
   if(re.test(input)) return 'Naver D2'
   return 'Naver Whale'
 }
