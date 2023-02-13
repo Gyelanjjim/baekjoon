da2f69a05525c391a8fd22986725c9c967417b6a
Gyelanjjim <108852943+Gyelanjjim@users.noreply.github.com>
2023-02-13 11:10:19 +0900

    Create FBI-RegExp.js
    [Bronze III] Title: FBI, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/2857. FBI/FBI-RegExp.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const solution = (input) => {
+  const re = new RegExp(/FBI/g)
+  let result = []
+  for(let i = 0; i<input.length; i++){
+    if(input[i].search(re) !== -1) result.push(i+1)
+  }
+  return result.length !== 0 ? result.join(' ') : "HE GOT AWAY!"
+}
+console.log(solution(input))
