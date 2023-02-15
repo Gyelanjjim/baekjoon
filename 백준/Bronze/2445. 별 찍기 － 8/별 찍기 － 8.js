499e6da2313091c3c55924ab5c97aed68dfdb4b8
Gyelanjjim <108852943+Gyelanjjim@users.noreply.github.com>
2023-02-15 17:54:03 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 116 ms, Memory: 9496 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.js
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.js
@@ -4,18 +4,17 @@ const input = fs.readFileSync('/dev/stdin').toString().trim()
 const solution = (input) => {
   let result = []
   for(let i = input; i>0; i--){
-    let side = "*".repeat(input-i)
-    let middle = ' '.repeat(i*2)
+    let side = "*".repeat(input-i+1)
+    let middle = ' '.repeat(i*2-2)
     result.push( side + middle + side )
   }
-  result.push('*'.repeat(input*2))
-  for(let i = 1; i<=input; i++){
+  for(let i = 1; i<input; i++){
     let side = "*".repeat(input-i)
     let middle = ' '.repeat(i*2)
     result.push( side + middle + side )
   }
   
-  return result.join('\n').trim()
+  return result.join('\n')
 }
 
 console.log(solution(input))
