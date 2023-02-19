6a0a7e85186df77393eec836696ce0cc3a6b9b03
Gyelanjjim <108852943+Gyelanjjim@users.noreply.github.com>
2023-02-20 01:41:35 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,26 +1,10 @@
-const sum = (obj, num) => {
-  let re = 0
-  for(let i = 0; i < num; i++){
-    re += obj[i] || 0
-  }
-  return re
-}
-
-const solution = (N, stages) => {
-  let len = N.toString().length
-  let count = {}
-  let fail = []
-  
-  stages.forEach(el => {
-    count[el] = ( count[el] || 0 ) + 1   
-  })
-  
-  for(let i = 1; i <= N; i++){
-    let j = ('0'.repeat(len-1) + i).slice(-len)
-    let rate = count[i] / (stages.length - sum(count, i)) || 0
-    let ele = fail.find(el => el[0] === rate )
-    ele ? ele.push(j) : fail.push([rate, j])      
-  }
-  
-  return fail.sort().reverse().map(el => el.slice(1).join(' ')).join(' ').split(' ').map(Number)
+function solution(N, stages) {
+    let result = [];
+    for(let i=1; i<=N; i++){
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        result.push([i, curr/reach]);
+    }
+    result.sort((a,b) => b[1] - a[1]);
+    return result.map((x) => x[0]);
 }
