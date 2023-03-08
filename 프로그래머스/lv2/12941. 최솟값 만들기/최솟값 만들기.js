a92a074897fe5efba321a214b4a8d5ab34ea1c8d
Gyelanjjim <108852943+Gyelanjjim@users.noreply.github.com>
2023-03-08 11:58:43 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.80 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -2,6 +2,6 @@ function solution(A,B){
     let sum = 0
     A = A.sort((a,b) => a-b)
     B = B.sort((a,b) => b-a)
-    A.forEach((el,i) => sum += A[i] * B[i] )
+    A.forEach((el,i) => sum += el * B[i] )
     return sum
 }
