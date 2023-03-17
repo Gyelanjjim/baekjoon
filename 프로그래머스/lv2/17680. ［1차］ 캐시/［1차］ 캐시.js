bdd7d65b77a70a230cd5073a8e6a50e1bfdf4758
Gyelanjjim <songcj92@gmail.com>
2023-03-18 01:28:56 +0900

    [level 2] Title: [1차] 캐시, Time: 0.85 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,22 +1,24 @@
 function solution(cacheSize, cities) {
-  let arr = Array(cacheSize).fill('')
-  let answer = 0
-  cities.forEach((el) => {
-    el = el.toLowerCase()
-    if(cacheSize === 0){
-      answer += 5  
-    }else{
-      const idx = arr.indexOf(el)
-      if(idx !== -1){
-        arr.splice(idx, 1)
-        arr.push(el)
-        answer += 1  
-      }else if(idx === -1){
-        arr.shift()
-        arr.push(el)
-        answer += 5
-      }
-    }
-  })
-  return answer
+    const map = new Map();
+  
+    const cacheHit = (city, map) => {
+        map.delete(city);
+        map.set(city, city);
+        return 1;
+    };
+  
+    const cacheMiss = (city, map, size) => {
+        if(size === 0) return 5;
+        (map.size === size) && map.delete(map.keys().next().value);
+        map.set(city, city); 
+        return 5;
+    };
+  
+    const getTimeCache = (city, map, size) => 
+    	(map.has(city) ? cacheHit : cacheMiss)
+    	(city, map, size);
+    
+  	return cities.map(city => 
+             	getTimeCache(city.toLocaleLowerCase(), map, cacheSize)
+                     ).reduce( (a, c) => a + c, 0);
 }
