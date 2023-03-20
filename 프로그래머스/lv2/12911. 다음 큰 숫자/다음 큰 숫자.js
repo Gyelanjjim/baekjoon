d5030fac5cc0f7caa6589a7d3bbbc1cc1bf083ff
Gyelanjjim <songcj92@gmail.com>
2023-03-20 10:50:03 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    const nOne = n.toString(2).replaceAll(0,'').length
+    let biggerOne = 0;
+    let biggerN = 0;
+    do{
+        n++
+        biggerN = n.toString(2)
+        biggerOne = biggerN.replaceAll(0,'').length
+    }while(nOne !== biggerOne)
+    
+    return parseInt(biggerN,2);
+}
