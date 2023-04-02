73d009c699aaa1a0afa44bda24faeff2b3f9c1b8
Gyelanjjim <songcj92@gmail.com>
2023-04-03 01:04:35 +0900

    [Bronze I] Title: 약수, Time: 136 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.js
+++ 백준/Bronze/1037. 약수/약수.js
@@ -4,7 +4,7 @@ const [count, input] = fs.readFileSync('/dev/stdin').toString().trim()
 
 const solution = (count, input) => {
   input = input.split(' ').sort((a, b) => a - b)
-  return count === '1' ? input ** 2 : input[0] * input[count-1]
+  return input[0] * input[count-1]
 }
 
 console.log(solution(count, input))
