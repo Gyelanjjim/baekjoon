c8a48a6223d504122a67ba277a7a55f6ec9031dd
Gyelanjjim <songcj92@gmail.com>
2023-04-25 18:37:22 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,14 +1,16 @@
 function solution(str_list) {
-  const l_idx = str_list.indexOf('l');
-  const r_idx = str_list.indexOf('r');
+    const l = str_list.indexOf('l')
+    const r = str_list.indexOf('r')
 
-  if (l_idx === -1 && r_idx === -1) {
-    return [];
-  } else if (l_idx !== -1 && r_idx === -1) {
-    return str_list.slice(0, l_idx);
-  } else if (l_idx === -1 && r_idx !== -1) {
-    return str_list.slice(r_idx + 1);
-  } else {
-    return l_idx < r_idx ? str_list.slice(0, l_idx) : str_list.slice(r_idx + 1);
-  }
+    if(r === -1 && l === -1) {
+        return []
+    }else if(r !== -1 && l !== -1){
+        return l < r ? str_list.slice(0,l) : str_list.slice(r+1)
+    }else if(r !== -1){
+        return str_list.slice(r+1)
+    }else{
+        return str_list.slice(0,l)
+    }
+    
 }
+
