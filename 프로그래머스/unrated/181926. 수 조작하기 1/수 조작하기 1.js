ca924f45df3ce22dc3da3f7e49ea0219dade4dcc
Gyelanjjim <songcj92@gmail.com>
2023-04-25 10:24:52 +0900

    [unrated] Title: 수 조작하기 1, Time: 2.13 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,11 +1,11 @@
 function solution(n, control) {
-    for(let i = 0 ; i < control.length ; i++){
-        switch(control[i]) {
-            case "w" : n++;break;
-            case "s" : n--;break;
-            case "d" : n+=10;break;
-            case "a" : n-=10;break;
-        }
+    const cmd = {
+        'w': (x) => x+1,
+        's': (x) => x-1,
+        'd': (x) => x+10,
+        'a': (x) => x-10
     }
-    return n;
+    control.split('').forEach(el => n = cmd[el](n))
+    
+    return n
 }
