98330043d93be05a77b65526cbc19c8c612d4e97
Gyelanjjim <songcj92@gmail.com>
2023-04-25 16:33:33 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,9 @@
+function solution(myStr) {
+    const arr = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+    const obj = {}
+    arr.forEach(el => obj[el] = 0)
+    for(let v of myStr){
+        obj[v] += 1  
+    }
+    return Object.values(obj)
+}
