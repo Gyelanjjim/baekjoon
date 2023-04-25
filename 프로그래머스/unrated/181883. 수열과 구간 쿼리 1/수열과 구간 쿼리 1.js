e9fce43032a663ef2e042564a7ad2941f8e509a6
Gyelanjjim <songcj92@gmail.com>
2023-04-25 13:09:37 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 2.55 ms, Memory: 35.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -0,0 +1,9 @@
+function solution(arr, queries) {
+    queries.forEach(el => {
+        const [s,e] = el
+        for(let i=s; i<=e; i++){
+            arr[i]++
+        }
+    })
+    return arr
+}
