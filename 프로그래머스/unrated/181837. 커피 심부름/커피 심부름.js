1b303fb150e44eb2c08dccae480a57dcadf41065
Gyelanjjim <songcj92@gmail.com>
2023-04-26 15:08:03 +0900

    [unrated] Title: 커피 심부름, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -1,3 +1,3 @@
 function solution(order) {
-    return order.map(v => v.match(/am|ng/g) ? 4500 : 5000).reduce((a,b)=>a+b,0)
+    return order.map(v => v.match(/f/g) ? 5000 : 4500).reduce((a,b)=>a+b,0)
 }
