ed5c5f6e02510f4f4c4d028e8912c9adc2af1b3e
Gyelanjjim <songcj92@gmail.com>
2023-05-07 22:51:36 +0900

    [unrated] Title: 배열 만들기 2, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,19 +1,5 @@
 function solution(l,r){
   const length = 63
-  const arr = Array.from({length}, (_, i) => (i+1).toString(2)*5);
-  let [s, e] = [-1,-1]
-  for(let i=0; i<length;i++){
-    if(arr[i] >= l){
-      s = i;
-      break;
-    } 
-  }
-  for(let i=length-1; i>=0;i--){
-    if(arr[i] <= r){
-      e = i;
-      break;
-    } 
-  }
-  
-  return arr.slice(s,e+1).length === 0 ? [-1] : arr.slice(s,e+1) 
+  const arr = Array.from({length}, (_, i) => (i+1).toString(2)*5).filter(el => el >= l && el <= r);
+  return arr.length ? arr : [-1]
 }
