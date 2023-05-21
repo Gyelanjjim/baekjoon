16227ac89687688a1004d91c6037b333d2cb06e4
Gyelanjjim <songcj92@gmail.com>
2023-05-21 21:17:47 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,6 +1,9 @@
 function solution(a, b) {
-    const month = [31,29,31,30,31,30,31,31,30,31,30,31]
-    const week = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
-    const na = month.slice(0,a-1).reduce((ac,v)=>ac+v,b) % 7
-    return week[na]   
+    let week = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    let date = new Date(`2016-${a}-${b}`).getDay()
+    return week[date];
+    // const month = [31,29,31,30,31,30,31,31,30,31,30,31]
+    // const week = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    // const na = month.slice(0,a-1).reduce((ac,v)=>ac+v,b) % 7
+    // return week[na]   
 }
