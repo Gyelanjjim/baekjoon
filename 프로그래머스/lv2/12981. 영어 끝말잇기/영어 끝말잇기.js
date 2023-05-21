b59849af41c1b75b4274086cff9ac59ca7fc2b1b
Gyelanjjim <songcj92@gmail.com>
2023-05-21 19:54:18 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,14 +1,11 @@
 function solution(n, words) {
-    let c = 1   // 회차
-    const w = words[0]  // 첫번째 단어
-    let list = [w]  // 앞서 말한 단어 리스트
+    let c = 1   // 회차  
+    let list = [words[0]]  // 앞서 말한 단어 리스트
     
     for(let i=1; i<words.length; i++){
         if(i%n === 0) c++
-        if(words[i-1][words[i-1].length-1] !== words[i][0]){
+        if(words[i-1][words[i-1].length-1] !== words[i][0] || list.includes(words[i])){
             return [i%n+1, c]
-        }else if(list.includes(words[i])){
-            return [i%n+1, c]            
         }
         list.push(words[i])
     }
