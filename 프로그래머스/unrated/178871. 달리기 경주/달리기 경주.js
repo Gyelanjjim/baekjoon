3584898ac154a5678ac8837b49a01e7ba3ade25e
Gyelanjjim <songcj92@gmail.com>
2023-05-25 01:42:00 +0900

    [unrated] Title: 달리기 경주, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,28 @@
+// function solution(players, callings) {
+//     callings.forEach(v=>{
+//         const x = players.indexOf(v)
+//         let a = players[x-1]  
+//         players[x-1] = players[x]
+//         players[x] = a
+//     })
+//     return players
+// }
+function solution(players, callings) {
+    let idx;
+    let name1;
+    let name2;
+    const idxList = {}
+
+    players.forEach((name,index)=>idxList[name]=index)
+    for(let call of callings){
+        idx = idxList[call]
+        name1 = players[idx]
+        name2 = players[idx-1]
+        idxList[call]-=1
+        idxList[name2]+=1
+        players[idx] = name2
+        players[idx-1] = name1
+    }    
+
+    return players;
+}
