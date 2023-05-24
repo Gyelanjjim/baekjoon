fcfef157071f7a9a292f3836688fb8eccd8177dc
Gyelanjjim <songcj92@gmail.com>
2023-05-25 02:04:30 +0900

    [unrated] Title: 달리기 경주, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,28 +1,16 @@
-// function solution(players, callings) {
-//     callings.forEach(v=>{
-//         const x = players.indexOf(v)
-//         let a = players[x-1]  
-//         players[x-1] = players[x]
-//         players[x] = a
-//     })
-//     return players
-// }
 function solution(players, callings) {
-    let idx;
-    let name1;
-    let name2;
-    const idxList = {}
-
-    players.forEach((name,index)=>idxList[name]=index)
-    for(let call of callings){
-        idx = idxList[call]
-        name1 = players[idx]
-        name2 = players[idx-1]
-        idxList[call]-=1
-        idxList[name2]+=1
-        players[idx] = name2
-        players[idx-1] = name1
-    }    
+    // const p = players.reduce((v,c,i)=>{return {...v,[c]:i}}, {})
+    const p = {}
+    players.forEach((v,i)=>p[v]=i)
+    for(let v of callings){
+        let idx = p[v]              // 호명된 플레이어의 현재 위치
+        let name1 = players[idx]    // 호명된 플레이어 이름
+        let name2 = players[idx-1]  // 호명된 플레이어의 앞사람 이름
+        p[v]-=1                     // 호명된 플레이어의 다음 위치(전진)
+        p[name2]+=1                 // 호명된 플레이어 앞사람의 다음 위치(후진)
+        players[idx] = name2        // 플레이어 순서 변동
+        players[idx-1] = name1      // 플레이어 순서 변동
+    }
 
     return players;
 }
