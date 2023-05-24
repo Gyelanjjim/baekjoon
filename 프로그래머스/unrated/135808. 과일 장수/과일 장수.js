e1b651c91719a1232bce23ea5b61c18b965b72ff
Gyelanjjim <songcj92@gmail.com>
2023-05-24 15:48:25 +0900

    [unrated] Title: 과일 장수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,8 +1,13 @@
 function solution(k, m, score) {
     let sum = 0
-    score.sort((a,b)=>b-a)
+//     score.sort((a,b)=>b-a)
     
-    for (let i=m-1; i<score.length; i+=m){
+//     for (let i=m-1; i<score.length; i+=m){
+//         sum += score[i] * m;
+//     }
+    score.sort((a,b)=>a-b)
+    const s = score.length % m
+    for (let i=s; i<score.length; i+=m){
         sum += score[i] * m;
     }
     return sum
