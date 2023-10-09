b2e8e8a5b8b2ea333b1d0621e6d1d887976da182
Gyelanjjim <songcj92@gmail.com>
2023-10-10 01:26:29 +0900

    [level 2] Title: 전화번호 목록, Time: 100.98 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,6 @@
+function solution(phoneBook) {
+    return !phoneBook.sort().some((_, i) => {
+        if(i === phoneBook.length - 1) return false;
+        return phoneBook[i+1].startsWith(phoneBook[i]);   
+    })
+}
