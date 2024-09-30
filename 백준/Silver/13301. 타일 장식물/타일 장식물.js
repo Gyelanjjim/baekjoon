a667e1d119d8b32bcbf2f034edfb986625268ab5
Gyelanjjim <songcj92@gmail.com>
2024-09-30 22:18:46 +0900

    [Silver V] Title: 타일 장식물, Time: 96 ms, Memory: 9332 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13301. 타일 장식물/타일 장식물.js
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.js
@@ -3,12 +3,16 @@ const input = fs.readFileSync('/dev/stdin').toString().trim();
 
 const solution = (input) => {
   const num = +input;
-  const arr = [0, 1];
+  let a = 0n;
+  let b = 1n;
+  let tmp;
   for (let i = 0; i<num-1; i++) {
-    const sum = arr[arr.length-2] + arr[arr.length-1];
-    arr.push(sum)
+    tmp = a;
+    a = b;
+    b += tmp;
   }
-  return (2 * arr[arr.length-1] + arr[arr.length-2]) * 2
+  return (4n * b + 2n * a).toString();  
 };
 
 console.log(solution(input));
+
